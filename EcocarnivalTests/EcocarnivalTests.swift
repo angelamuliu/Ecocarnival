92613a7266117984325cdb4c11f4dce168bed95c
Angela Liu <angelamuliu@gmail.com>
2015-11-09 11:39:33 -0500

    Initial Commit

---
+++ EcocarnivalTests/EcocarnivalTests.swift
@@ -0,0 +1,36 @@
+//
+//  EcocarnivalTests.swift
+//  EcocarnivalTests
+//
+//  Created by Angela Liu on 11/9/15.
+//  Copyright © 2015 amliu. All rights reserved.
+//
+
+import XCTest
+@testable import Ecocarnival
+
+class EcocarnivalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
